6b87dd4cd95067dcfb052f4de167b554eef06ca2
fabio.hafreire <fabio.hafreire@sp.senac.br>
2022-11-19 14:45:51 -0300

    Atualização da documentação da aplicação

--- app/src/main/java/br/senac/lojaretrofit/services/API.kt
+++ app/src/main/java/br/senac/lojaretrofit/services/API.kt
@@ -6,17 +6,21 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-class API(context: Context) {
-
+/** Classe para centralizar os serviços do Retrofit **/
+class API() {
+    //Insância principal do Retrofit, que configura a URL base e o conversor
+    //dos arquvios de texto que são mandados/recebidos para/do backend
     private val retrofit: Retrofit
         get() {
-                     return Retrofit
+            return Retrofit
                 .Builder()
                 .baseUrl("http://oficinacordova.azurewebsites.net/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
         }
 
+    //Instância do serviço do produto, concentra as funções
+    //mapeadas para chamadas de endpoints de produto no back end
     val produto: ProdutoService
         get() {
             return retrofit.create(ProdutoService::class.java)
